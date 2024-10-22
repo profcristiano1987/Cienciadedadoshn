7cf4c2b2c8c104f7c8f2681d892ea0f4a2a18fde
Cristiano Santos Rodrigues <107894186+profcristiano1987@users.noreply.github.com>
2024-10-22 20:37:10 -0300

    ok

--- GRAFICOS/informacoesGlobais.js
+++ GRAFICOS/informacoesGlobais.js
@@ -6,7 +6,8 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML = 'Com uma população global de quase , os esportes têm uma presença marcante no cotidiano das pessoas.3 bilhões praticam esportes regularmente, dedicando em média 2 horas por semana para cuidar da saúde e melhorar seu bem-estar. Além disso, 4 bilhões de pessoas acompanham eventos esportivos, tornando o esporte uma paixão que une pessoas de todas as culturas e idades. Esses números mostram que o esporte é muito mais do que atividade física ou entretenimento; é um fenômeno global que conecta e inspira'
+
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
