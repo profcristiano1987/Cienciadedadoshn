5b4f25ce86a1a917b9d6dde773d11ec312c34277
Cristiano Santos Rodrigues <107894186+profcristiano1987@users.noreply.github.com>
2024-10-22 19:57:40 -0300

    alteração

--- GRAFICOS/informacoesGlobais.js
+++ GRAFICOS/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
+const url='https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/esportes/esportes-dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
