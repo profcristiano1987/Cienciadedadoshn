3bb1c19f5252b38aeb850497c516557bbd221b84
Cristiano Santos Rodrigues <107894186+profcristiano1987@users.noreply.github.com>
2024-10-29 20:29:17 -0300

    ok

--- GRAFICOS/informacoesGlobais.js
+++ GRAFICOS/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url= 'https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/educacao/educacao-dados-globais.json'
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
@@ -6,7 +6,8 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de ${dados.total_pessoas_mundo} de pessoas e que o  total de pessoas com acesso à educação é de ${dados.total_pessoas_com_acesso_a_educacao}? O tempo médio gasto por dia estudando é de ${dados.tempo_medio_dia_estudando} horas e o total de pessoas com educação superior é de ${dados.total_pessoas_com_educacao_superior}.  Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?`
+    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de ${dados.total_pessoas_mundo} de pessoas e que o  total de pessoas com acesso à educação é de ${dados.total_pessoas_com_acesso_a_educacao}? O tempo médio gasto por dia estudando é de ${dados.tempo_medio_dia_estudando} horas e o total de pessoas com educação superior é de ${dados.total_pessoas_com_educacao_superior}.  Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?
+`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
