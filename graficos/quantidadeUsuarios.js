5fc29a70b004f6f41d2965cd65576293025a1e13
Cristiano Santos Rodrigues <107894186+profcristiano1987@users.noreply.github.com>
2024-11-12 20:03:32 -0300

    ok

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,25 +1,22 @@
-https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json
-
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
-  
-  const data = [
-    {
-      x: nomeDasRedes,
-      y: quantidadeUsuarios,
-      type: 'bar'
-    }
-  ]
-  
-  const grafico = document.createElement('div')
-  grafico.className = 'grafico'
-  document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
-  
-  }
-  
-  quantidadeUsuarios()
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar'
+        }
+    ]
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuariosPorRede()
